03072a0758a6b07d0a780f175e7eb8f0edf3257c
robwayne <robwaynegordon@gmail.com>
2017-07-15 01:46:03 -0500

    Added Slideout Menu

---
+++ client/helpers/layout.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { Slideout } from 'meteor/patrickleet:slideout';
+
+Template.layout.onRendered(function(){
+  const template = this;
+
+  slideoutInstance = new Slideout({
+    'menu':template.$('#side-menu').get(0),
+    'panel':template.$('#main-content').get(0),
+    'padding':256,
+    'tolerance':70,
+    'grabWidth':180
+  });
+  console.log(slideoutInstance);
+
+});
